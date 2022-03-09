e7d3c3b60da8523d4517e3b238691afbfe8b3505
rzywczak <73279676+rzywczak@users.noreply.github.com>
2022-03-09 18:36:30 +0100

    Przycisk i opcja wybrania pliku z dysku

--- src/main/java/com/example/tachographanalysis/DigitalAnalysisController.java
+++ src/main/java/com/example/tachographanalysis/DigitalAnalysisController.java
@@ -6,17 +6,93 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.TransferMode;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import com.example.tachographanalysis.size.SizeController;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class DigitalAnalysisController {
     @FXML
     private Button btnBack, btnUpload;
+    @FXML
     private Label uploadText;
+    @FXML
+    private Button dragOver;
+    @FXML
+    private File file;
+    @FXML
+    private String DDDFile;
+    String text = "Choose file from memory or drag and drop here";
+    List<String> lstFile;
 
     public void getBack() throws Exception {
         Parent fxmlLoader = FXMLLoader.load(getClass().getResource("main.fxml"));
         Stage scene = (Stage) btnBack.getScene().getWindow();
         scene.setScene(new Scene(fxmlLoader, SizeController.sizeW,SizeController.sizeH));
     }
+
+    @FXML
+    private void handleDragOverButton(DragEvent event) {
+        if(event.getDragboard().hasFiles()) {
+            event.acceptTransferModes(TransferMode.ANY);
+        }
+        dragOver.setText("Upuść tutaj");
+    }
+
+
+    @FXML
+    private void onDragClickedButton() throws MalformedURLException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("DDD Files", "*.ddd", "*.DDD"));
+        File selectedFile = fileChooser.showOpenDialog(null);
+        DDDFile = selectedFile.toURI().toURL().toString();
+        System.out.println(DDDFile);
+    }
+
+    @FXML
+    private void handleDroppedButton(DragEvent event) throws FileNotFoundException, MalformedURLException {
+        List<File> files = event.getDragboard().getFiles();
+        List<String> validExtensions = Arrays.asList("ddd","DDD");
+        file = new File(String.valueOf(new FileInputStream(files.get(0))));
+        if(!validExtensions.containsAll(event.getDragboard()
+                .getFiles().stream().map(file -> getExtension(file.getName()))
+                .collect(Collectors.toList()))) {
+            dragOver.setText("To nie jest plik .ddd");
+        }else {
+            files.forEach((file -> System.out.println(file.getAbsolutePath())));
+        }
+    }
+
+
+
+    public String getExtension(String fileName){
+        String extension = "";
+
+        int i = fileName.lastIndexOf('.');
+        if (i > 0 && i < fileName.length() - 1)
+            return fileName.substring(i + 1).toLowerCase();
+
+        return extension;
+    }
+
+    public void initialize(URL url, ResourceBundle rb) {
+        lstFile = new ArrayList<>();
+        lstFile.add("*.ddd");
+        lstFile.add("*.DDD");
+    }
+
 }
