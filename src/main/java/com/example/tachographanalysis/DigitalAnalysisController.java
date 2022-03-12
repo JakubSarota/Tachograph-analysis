61ecd01c4048312b4d08e10de798810c65104ad0
rzywczak <73279676+rzywczak@users.noreply.github.com>
2022-03-12 13:44:43 +0100

    Otwieranie pliku txt i wyświetlanie zawartości

--- src/main/java/com/example/tachographanalysis/DigitalAnalysisController.java
+++ src/main/java/com/example/tachographanalysis/DigitalAnalysisController.java
@@ -1,30 +1,37 @@
 package com.example.tachographanalysis;
 
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import com.example.tachographanalysis.size.SizeController;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+
+import javax.xml.transform.ErrorListener;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.io.File;
+import java.util.ResourceBundle;
+import java.util.Scanner;
 
-public class DigitalAnalysisController {
+
+public class DigitalAnalysisController implements Initializable{
+
+    @FXML
+    private TextArea textArea;
     @FXML
     private Button btnBack, btnUpload;
     @FXML
@@ -35,7 +42,7 @@ public class DigitalAnalysisController {
     private File file;
     @FXML
     private String DDDFile;
-    String text = "Choose file from memory or drag and drop here";
+//    String text = "Choose file from memory or drag and drop here";
     List<String> lstFile;
 
     public void getBack() throws Exception {
@@ -54,26 +61,45 @@ public class DigitalAnalysisController {
 
 
     @FXML
-    private void onDragClickedButton() throws MalformedURLException {
+     void onDragClickedButton(MouseEvent event) {
+//
         FileChooser fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("DDD Files", "*.ddd", "*.DDD"));
-        File selectedFile = fileChooser.showOpenDialog(null);
-        DDDFile = selectedFile.toURI().toURL().toString();
-        System.out.println(DDDFile);
+                new FileChooser.ExtensionFilter("DDD Files", "*.ddd", "*.DDD", "*.txt"));
+        File file = fileChooser.showOpenDialog(new Stage());
+        try{
+            Scanner scanner = new Scanner(file);
+//            System.out.println(file);
+            while(scanner.hasNextLine()){
+                textArea.appendText(scanner.nextLine() + "\n");
+            }
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
     }
 
+
     @FXML
-    private void handleDroppedButton(DragEvent event) throws FileNotFoundException, MalformedURLException {
+    private void handleDroppedButton(DragEvent event) throws FileNotFoundException {
         List<File> files = event.getDragboard().getFiles();
-        List<String> validExtensions = Arrays.asList("ddd","DDD");
+        List<String> validExtensions = Arrays.asList("ddd","DDD","txt");
         file = new File(String.valueOf(new FileInputStream(files.get(0))));
         if(!validExtensions.containsAll(event.getDragboard()
                 .getFiles().stream().map(file -> getExtension(file.getName()))
                 .collect(Collectors.toList()))) {
             dragOver.setText("To nie jest plik .ddd");
-        }else {
-            files.forEach((file -> System.out.println(file.getAbsolutePath())));
+        }
+        else {
+            try{
+                Scanner scanner = new Scanner(files.get(0));
+//            System.out.println(file);
+                while(scanner.hasNextLine()){
+                    textArea.appendText(scanner.nextLine() + "\n");
+                    dragOver.setText("Poprawnie załadowano plik");
+                }
+            } catch (FileNotFoundException e){
+                e.printStackTrace();
+            }
         }
     }
 
@@ -88,11 +114,17 @@ public class DigitalAnalysisController {
 
         return extension;
     }
-
+    @Override
     public void initialize(URL url, ResourceBundle rb) {
         lstFile = new ArrayList<>();
         lstFile.add("*.ddd");
         lstFile.add("*.DDD");
     }
+
+
+
+
+
+
 
 }
