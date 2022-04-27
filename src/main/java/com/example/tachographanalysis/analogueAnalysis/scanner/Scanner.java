27c92cbe7667b8d9ba0d4c2ac93774447e9353c4
JakubSarota <72095085+JakubSarota@users.noreply.github.com>
2022-04-27 19:54:24 +0200

    Przekierowanie do pliku pdf

--- src/main/java/com/example/tachographanalysis/analogueAnalysis/scanner/Scanner.java
+++
@@ -1,5 +0,0 @@
-package com.example.tachographanalysis.analogueAnalysis.scanner;
-
-public class Scanner {
-
-}
---
+++ src/main/java/com/example/tachographanalysis/database/loadExistData/AnalogueAnalysis/OpenAnalogueData.java
@@ -0,0 +1,5 @@
+package com.example.tachographanalysis.database.loadExistData.AnalogueAnalysis;
+
+public class OpenAnalogueData {
+
+}
